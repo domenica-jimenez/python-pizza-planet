87a3eb01f1e01295b48d034a489757609db073d4
Doménica Jiménez <domenica.jimenez@ioet.com>
2024-04-14 01:42:16 -0500

    feat/DJ: PIZ3 implement template logic to services

--- app/services/size.py
+++ app/services/size.py
@@ -1,37 +1,4 @@
-from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint, jsonify, request
-
+from app.services.templates.product_service import ProductService
 from ..controllers import SizeController
 
-size = Blueprint('size', __name__)
-
-
-@size.route('/', methods=POST)
-def create_size():
-    size, error = SizeController.create(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
-
-
-@size.route('/', methods=PUT)
-def update_size():
-    size, error = SizeController.update(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
-
-
-@size.route('/id/<_id>', methods=GET)
-def get_size_by_id(_id: int):
-    size, error = SizeController.get_by_id(_id)
-    response = size if not error else {'error': error}
-    status_code = 200 if size else 404 if not error else 400
-    return jsonify(response), status_code
-
-@size.route('/', methods=GET)
-def get_sizes():
-    ingredients, error = SizeController.get_all()
-    response = ingredients if not error else {'error': error}
-    status_code = 200 if ingredients else 404 if not error else 400
-    return jsonify(response), status_code
+size = ProductService(SizeController).template_service('size', __name__)
